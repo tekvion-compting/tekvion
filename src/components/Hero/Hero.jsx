5fbc6de3e595dbdaf41f23f5f6f9382b62667583
TalifhaniTekvion <talifhani.tshifura@tekvion.co.za>
2025-11-22 19:52:57 +0200

    feat(hero): add full-width hero section with layered background and CTA

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,42 @@
 import "./Hero.css";
+import heroLogo from "../../assets/tekvion-image.png";
+import heroBg from "../../assets/hero-bg.png";
+import heroCurve from "../../assets/hero-curve.png";
 
 export default function Hero() {
   return (
     <section className="hero">
-      <h2>Hero Section</h2>
+      <div
+        className="hero-bg"
+        style={{ backgroundImage: `url(${heroBg})` }}
+      >
+        <div className="container hero-content">
+
+          <div className="hero-left">
+            <h1 className="hero-title">
+              Smart Tech,<br /> Simple Solutions
+            </h1>
+
+            <p className="hero-subtext">
+              Your all-in-one partner for software, IT solutions, 
+              and digital innovation.
+            </p>
+
+            <a href="#get-started" className="hero-btn">
+              Work With Us
+            </a>
+          </div>
+
+          <div className="hero-right">
+            <img src={heroLogo} alt="Tekvion Logo" className="hero-image" />
+          </div>
+
+        </div>
+
+        <img src={heroCurve} alt="curve" className="hero-curve" />
+      </div>
+
     </section>
   );
 }
+
